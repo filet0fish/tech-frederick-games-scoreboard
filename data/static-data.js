97c101ae206b59941c93efb0c832f6e4a2bad7b9
Josh Trout <jtrout@gannett.com>
2018-08-11 15:06:53 -0400

    Static data store

---
+++ data/static-data.js
@@ -0,0 +1,49 @@
+var teams = [
+    {
+        id: 1,
+        name: "Team 1",
+        score: 0
+    }
+];
+var games = [
+    {
+        id: 1,
+        name: 'Crazy Catapult Cornhole'
+    },
+    {
+        name: 'Drop It Like Its Hot'
+    },
+    {
+        name: 'Meltdown'
+    },
+    {
+        name: 'Monster Jenga'
+    },
+    {
+        name: 'Monster Scrabble'
+    },
+    {
+        name: 'Rapid Fire'
+    },
+    {
+        name: 'Soccer Darts'
+    },
+    {
+        name: 'Splash! Volleyball'
+    },
+    {
+        name: 'Tech Trike-athlon'
+    },
+    {
+        name: 'Xtreme Laser Tag'
+    }
+];
+var scores = [
+    {
+        team: 1,
+        game: 1,
+        score: 42
+    }
+];
+
+module.exports = { teams: teams, games: games, scores: scores };
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var staticData = require('../data/static-data');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  staticData.teams[0].score += 1;
+
   res.render('index', {
-    teams : [
-      {
-        index: 1,
-        name: 'Team1',
-        score: 100
-      },
-      {
-        index: 2,
-        name: 'Team2',
-        score: 200
-      }
-    ]
-});
+    teams : staticData.teams
+  });
 });
 
 module.exports = router;
