1cb9fd7acc3f2d94d14d1b4f41033ac9a402574f
Josh Trout <jtrout@gannett.com>
2018-08-11 13:46:01 -0400

    Todo

--- routes/teams.js
+++ routes/teams.js
@@ -2,25 +2,60 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/:index', function(req, res, next) {
-  res.render('teams', {team:{
-    name: 'Team1',
-    score: 100,
-    index: req.param('index')
-},
-    games: [
-        {name: 'Crazy Catapult Cornhole', score:5},
-        {name: 'Drop It Like Its Hot', score: 5},
-        {name: 'Meltdown', score: 5},
-        {name: 'Monster Jenga', score: 5},
-        {name: 'Monster Scrabble', score: 5},
-        {name: 'Rapid Fire', score: 5},
-        {name: 'Soccer Darts', score: 5},
-        {name: 'Splash! Volleyball', score: 5},
-        {name: 'Tech Trike-athlon', score: 5},
-        {name: 'Xtreme Laser Tag', score: 5}
-    ]
-})
+router.get('/:index', function (req, res, next) {
+
+    var data = {
+        team: {
+            name: 'Team1',
+            score: 0,
+            index: req.param('index')
+        },
+        games: [{
+                name: 'Crazy Catapult Cornhole',
+                score: 5
+            },
+            {
+                name: 'Drop It Like Its Hot',
+                score: 5
+            },
+            {
+                name: 'Meltdown',
+                score: 5
+            },
+            {
+                name: 'Monster Jenga',
+                score: 5
+            },
+            {
+                name: 'Monster Scrabble',
+                score: 5
+            },
+            {
+                name: 'Rapid Fire',
+                score: 5
+            },
+            {
+                name: 'Soccer Darts',
+                score: 5
+            },
+            {
+                name: 'Splash! Volleyball',
+                score: 5
+            },
+            {
+                name: 'Tech Trike-athlon',
+                score: 5
+            },
+            {
+                name: 'Xtreme Laser Tag',
+                score: 5
+            }
+        ]
+    };
+
+    // Loop through data.games and add to data.team.score
+
+    res.render('teams', data);
 });
 
 module.exports = router;
